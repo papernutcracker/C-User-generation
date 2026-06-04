2d9d2cc3ffd453807f2a7ac7da3fc61fa0b35b28
papernutcracker <papernutcracker@gmail.com>
2026-06-04 15:04:28 +0300

    Add User class with properties and ToString method

---
+++ User.cs
@@ -0,0 +1,16 @@
+namespace FakeUserApp
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}\nPhone: {PhoneNumber}\nEmail: {Email}\nAddress: {Address}";
+        }
+    }
+}
